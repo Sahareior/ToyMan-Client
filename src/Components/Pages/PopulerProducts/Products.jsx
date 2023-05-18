8f78d419ebfff89a5b1e8a109186f6e5a2a153bb
mdAshaduzzaman1 <ashaduzzamannoor19@gmail.com>
2023-05-18 14:40:14 +0600

    fixed some product sections bug

--- src/Components/Pages/PopulerProducts/Products.jsx
+++ src/Components/Pages/PopulerProducts/Products.jsx
@@ -13,18 +13,18 @@ const Products = () => {
         <div>
             <h1 className='text-center text-5xl font-bold'>Customer Loves</h1>
             <p className='text-center text-xl text-cyan-400'>Populer Products</p>
-            <div className="grid grid-cols-4 gap-7">
+            <div className="grid grid-cols-4 mt-5 gap-7">
                 {
                     products.map(data=> 
                         
                         <div key={data.length} className="card w-96 bg-base-100 shadow-xl">
-  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <figure><img src={data.photo} alt="Shoes" /></figure>
   <div className="card-body">
     <h2 className="card-title">
-      Shoes!
+      {data.name}
       <div className="badge badge-secondary">NEW</div>
     </h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <p>{data.price}</p>
     <div className="card-actions justify-end">
       <div className="badge badge-outline">Fashion</div>
       <div className="badge badge-outline">Products</div>
